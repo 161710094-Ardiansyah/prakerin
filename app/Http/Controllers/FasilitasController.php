6d28a6305937ff55944575e33445f66fefd4e9e1
161710094-Ardiansyah <161710094-Ardiansyah@smkassalaambandung.sch.id>
2018-07-04 16:36:58 +0700

    first commit

---
+++ app/Http/Controllers/FasilitasController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\fasilitas;
+use Illuminate\Http\Request;
+
+class FasilitasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $penyalur = Penyalur::all();
+        return view('fasilitas.index',compact('fasilitas'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('fasilitas.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+         $this->validate($request,[
+            'judul' => 'required',
+            'deskripsi' => 'required',
+            'gambar' => 'required',
+            
+        ]);
+        $fasilitas = new fasilitas;
+        $fasilitas->judul = $request->judul;
+        $fasilitas->deskripsi = $request->deskripsi;
+        $fasilitas->gambar = $request->gambar;
+        $fasilitas->save();
+        return redirect()->route('penyalur.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\fasilitas  $fasilitas
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $fasilitas = fasilitas::findOrFail($id);
+        return view('fasilitas.show',compact('fasilitas'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\fasilitas  $fasilitas
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $penyalur = fasilitas::findOrFail($id);
+        return view('fasilitas.edit',compact('fasilitas'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\fasilitas  $fasilitas
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $this->validate($request,[
+            'judul' => 'required',
+            'deskripsi' => 'required',
+            'gambar' => 'required',
+            
+        ]);
+        $fasilitas = Penyalur::findOrFail($id);        
+        $fasilitas->judul = $request->judul;
+        $fasilitas->deskripsi = $request->deskripsi;
+        $fasilitas->gambar = $request->gambar;
+        $fasilitas->save();
+        return redirect()->route('fasilitas.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\fasilitas  $fasilitas
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $fasilitas = fasilitas::findOrFail($id);
+        $fasilitas->delete();
+        return redirect()->route('fasilitas.index');
+    }
+}
